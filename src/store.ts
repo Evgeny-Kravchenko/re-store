3c88636d4047f81aa827510fbba710307f1879e1
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-09-10 10:52:33 +0300

    feat: add an enhancer for the store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,18 @@
 import { createStore, Store } from 'redux';
 import reducer from './reducers';
 
-const store: Store = createStore(reducer);
+const enhancer = (createStore: any) => (reducer: any) => {
+  const store = createStore(reducer);
+  const originalDispatch = store.dispatch;
+  store.dispatch = <A>(action: A | string) => {
+    if (typeof action === 'string') {
+      return originalDispatch({ type: action });
+    }
+    return originalDispatch(action);
+  };
+  return store;
+};
+
+const store: Store = createStore(reducer, enhancer);
 
 export default store;
